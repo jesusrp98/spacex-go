247ac41269333b40caa61aa165453f245e37013c
LINUX-XPS <jesusrope98@outlook.com>
2018-12-18 12:49:04 +0100

    Added updated colors & urls

---
+++ lib/util/colors.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+/// COLORS FILE
+/// It contains app colors to used them all around the app
+const primaryColor = const Color(0xFF151515);
+const accentColor = const Color(0xFFFFBC02D);
+const backgroundColor = const Color(0xFF212121);
+const cardColor = const Color(0xFF2C2C2C);
+
+const primaryText = const Color(0xFFFFFFFF);
+const secondaryText = const Color(0xFFBCBCBC);
+const lateralText = const Color(0xFF828282);
+
+const acceptIcon = const Color(0xFF4CAF50);
+const denyIcon = const Color(0xFFF44336);
+const nullIcon = const Color(0xFF607D8B);
+
+const highlightColor = const Color(0xFF424242);
+const dividerColor = const Color(0xFF616161);
+
+const locationPin = const Color(0xFFD32F2F);
+
+const disabledFab = const Color(0xFF757575);
