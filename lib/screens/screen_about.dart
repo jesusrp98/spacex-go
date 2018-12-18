6e9b0ec42a65c5a7696410218b21115c4590d5d5
LINUX-PC <jesusrope98@outlook.com>
2018-12-18 23:05:35 +0100

    Improving about screen

--- lib/screens/screen_about.dart
+++ lib/screens/screen_about.dart
@@ -4,9 +4,10 @@ import 'package:package_info/package_info.dart';
 
 import '../util/colors.dart';
 import '../util/url.dart';
+import '../widgets/separator.dart';
 
-/// ABOUT PAGE CLASS
-/// This class represent the about page. It contains a list with useful
+/// ABOUT SCREEN
+/// This view contains a list with useful
 /// information about the app & its developer.
 class AboutScreen extends StatefulWidget {
   @override
@@ -32,8 +33,6 @@ class _AboutScreenState extends State<AboutScreen> {
     setState(() => _packageInfo = info);
   }
 
-  final Future<PackageInfo> packageInfo = PackageInfo.fromPlatform();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,78 +42,94 @@ class _AboutScreenState extends State<AboutScreen> {
           ListTile(
             leading: const Icon(Icons.info_outline),
             title: const Text('SpaceX GO! - Launch Tracker'),
-            subtitle: Text('v${_packageInfo.version}:${_packageInfo.buildNumber} - beta'),
+            subtitle: Text(
+                'v${_packageInfo.version}:${_packageInfo.buildNumber} - beta'),
           ),
-          const Divider(indent: 72.0, height: 0.0),
+          Separator.divider(indent: 72.0, height: 0.0),
           ListTile(
             leading: const Icon(Icons.person_outline),
             title: const Text('Created by @jesusrp98'),
             subtitle: const Text('Reddit: u/jesusrp98'),
             onTap: () async => await FlutterWebBrowser.openWebPage(
-                url: Url.authorReddit, androidToolbarColor: primaryColor),
+                  url: Url.authorReddit,
+                  androidToolbarColor: primaryColor,
+                ),
           ),
-          const Divider(indent: 72.0, height: 0.0),
+          Separator.divider(indent: 72.0, height: 0.0),
           ListTile(
             leading: const Icon(Icons.star_border),
             title: const Text('Enjoying the app?'),
             subtitle:
                 const Text('Click here to leave your experience in the store'),
             onTap: () async => await FlutterWebBrowser.openWebPage(
-                url: Url.storePage, androidToolbarColor: primaryColor),
+                  url: Url.storePage,
+                  androidToolbarColor: primaryColor,
+                ),
           ),
-          const Divider(indent: 72.0, height: 0.0),
+          Separator.divider(indent: 72.0, height: 0.0),
           ListTile(
             leading: const Icon(Icons.mail_outline),
             title: const Text('Email me'),
             subtitle: const Text('Report a bug or request a feature'),
             onTap: () async => await FlutterWebBrowser.openWebPage(
-                url: Url.email, androidToolbarColor: primaryColor),
+                  url: Url.email,
+                  androidToolbarColor: primaryColor,
+                ),
           ),
-          const Divider(indent: 72.0, height: 0.0),
+          Separator.divider(indent: 72.0, height: 0.0),
           ListTile(
             leading: const Icon(Icons.apps),
             title: const Text('More from Chechu'),
             subtitle: const Text('Well designed, open-source apps'),
             onTap: () async => await FlutterWebBrowser.openWebPage(
-                url: Url.authorStore, androidToolbarColor: primaryColor),
+                  url: Url.authorStore,
+                  androidToolbarColor: primaryColor,
+                ),
           ),
-          const Divider(indent: 72.0, height: 0.0),
+          Separator.divider(indent: 72.0, height: 0.0),
           ListTile(
             leading: const Icon(Icons.people_outline),
             title: const Text('This is free software'),
             subtitle:
                 const Text('Source code is available in GitHub for everyone'),
             onTap: () async => await FlutterWebBrowser.openWebPage(
-                url: Url.cherryGithub, androidToolbarColor: primaryColor),
+                  url: Url.cherryGithub,
+                  androidToolbarColor: primaryColor,
+                ),
           ),
-          const Divider(indent: 72.0, height: 0.0),
+          Separator.divider(indent: 72.0, height: 0.0),
           ListTile(
-              leading: const Icon(Icons.public),
-              title: const Text('No imperial units?'),
-              subtitle: const Text(
-                  "Learn more about the 'International System of Units'"),
-              onTap: () async => await FlutterWebBrowser.openWebPage(
+            leading: const Icon(Icons.public),
+            title: const Text('No imperial units?'),
+            subtitle: const Text(
+                "Learn more about the 'International System of Units'"),
+            onTap: () async => await FlutterWebBrowser.openWebPage(
                   url: Url.internationalSystem,
-                  androidToolbarColor: primaryColor)),
-          const Divider(indent: 72.0, height: 0.0),
+                  androidToolbarColor: primaryColor,
+                ),
+          ),
+          Separator.divider(indent: 72.0, height: 0.0),
           ListTile(
             leading: const Icon(Icons.folder_open),
             title: const Text('App credits'),
             subtitle: const Text(
                 'Using open-source r/SpaceX REST API by @jakewmeyer'),
             onTap: () async => await FlutterWebBrowser.openWebPage(
-                url: Url.spacexGithub, androidToolbarColor: primaryColor),
+                  url: Url.spacexGithub,
+                  androidToolbarColor: primaryColor,
+                ),
           ),
-          const Divider(indent: 72.0, height: 0.0),
-          const SizedBox(height: 16.0),
+          Separator.divider(indent: 72.0, height: 0.0),
+          Separator.spacer(height: 16.0),
           Text(
             'This application is not affiliated in any way with SpaceX.\nSpaceX is a private trademark.',
-            style: Theme.of(context).textTheme.body1.copyWith(
-                  color: secondaryText,
-                ),
+            style: Theme.of(context)
+                .textTheme
+                .body1
+                .copyWith(color: secondaryText),
             textAlign: TextAlign.center,
           ),
-          const SizedBox(height: 16.0),
+          Separator.spacer(height: 16.0),
         ]),
       ),
     );
