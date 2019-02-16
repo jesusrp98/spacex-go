909261feb04e8881508bd4f3c273d1e245a21d66
LINUX-PC <jesusrope98@outlook.com>
2019-02-16 13:04:33 +0100

    Updated author link

--- lib/util/url.dart
+++ lib/util/url.dart
@@ -84,7 +84,7 @@ class Url {
   // About page
   static const String authorReddit = 'https://www.reddit.com/user/jesusrp98';
   static const String authorStore =
-      'https://play.google.com/store/apps/developer?id=Chechu';
+      'https://play.google.com/store/apps/developer?id=Jes%C3%BAs+Rodr%C3%ADguez+P%C3%A9rez';
   static const String cherryStore =
       'https://play.google.com/store/apps/details?id=com.chechu.cherry';
   static const String cherryGithub = 'https://github.com/jesusrp98/spacex-go';
