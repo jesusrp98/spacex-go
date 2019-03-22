0b6efcefadad71e2d04d4351ab9c7ee5c88947cd
LINUX-PC <jesusrope98@outlook.com>
2019-03-22 17:41:11 +0100

    Added comic sans & font refactor

--- lib/ui/screens/about.dart
+++ lib/ui/screens/about.dart
@@ -65,6 +65,10 @@ class _AboutScreenState extends State<AboutScreen> {
             'about.version.body',
             {'version': _packageInfo.version, 'status': 'release'},
           ),
+          onTap: () async => await FlutterWebBrowser.openWebPage(
+                url: 'https://gph.to/2TUnrj2',
+                androidToolbarColor: Theme.of(context).primaryColor,
+              ),
         ),
         Separator.divider(height: 0, indent: 72),
         ListCell(
