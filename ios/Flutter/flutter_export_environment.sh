c17f7a7413e3958641c19de176e38150ef4ea4c2
jesusrp98 <jesusrope98@outlook.com>
2019-11-16 16:24:48 +0100

    Updated ios launchscreen

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/jesus/.flutter-sdk"
-export "FLUTTER_APPLICATION_PATH=/home/jesus/Documents/spacex-go"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_ROOT=/Users/jesus/flutter-sdk"
+export "FLUTTER_APPLICATION_PATH=/Users/jesus/Documents/spacex-go"
+export "FLUTTER_TARGET=/Users/jesus/Documents/spacex-go/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/home/jesus/.flutter-sdk/bin/cache/artifacts/engine/ios"
-export "FLUTTER_BUILD_NAME=2.6.0"
-export "FLUTTER_BUILD_NUMBER=44"
+export "FLUTTER_FRAMEWORK_DIR=/Users/jesus/flutter-sdk/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=2.8.0"
+export "FLUTTER_BUILD_NUMBER=47"
