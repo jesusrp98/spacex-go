2f3378e782b0d1b2b341a8f3b86c7bc6440d17c1
LINUX-PC <jesusrope98@outlook.com>
2019-11-29 14:14:14 +0100

    Updated light theme

--- lib/util/colors.dart
+++ lib/util/colors.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 
 /// LIGHT THEME
 const lightPrimaryColor = Color(0xFF1E1E1E);
-const lightAccentColor = Color(0xFF3F51B5);
+const lightAccentColor = Color(0xFF3949AB);
 
 /// DARK THEME
 const darkPrimaryColor = Color(0xFF151515);
