30bde5a2599cd3d2c2c8e6547dfd3eecf8f54e7f
jesusrp98 <jesusrope98@outlook.com>
2020-04-06 12:08:11 +0200

    Improved error screen

--- lib/ui/screens/error.dart
+++ lib/ui/screens/error.dart
@@ -1,18 +1,18 @@
 import 'package:big_tip/big_tip.dart';
 import 'package:flutter/material.dart';
 
+/// Screen that is displayed when the routing system
+/// throws an error (404 screen).
 class ErrorScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-      ),
       body: BigTip(
         title: 'An error ocurred',
         subtitle: 'This page is not available',
-        child: Icon(Icons.sentiment_very_dissatisfied),
+        action: 'GO BACK',
+        actionCallback: () => Navigator.pop(context),
+        child: Icon(Icons.error_outline),
       ),
     );
   }
