d90a81e99c07af87b55fdb5980a02f25fb1205cb
jesusrp98 <jesusrope98@outlook.com>
2020-04-11 19:13:34 +0200

    Added new docs

--- lib/ui/widgets/sized_image.dart
+++ lib/ui/widgets/sized_image.dart
@@ -3,7 +3,8 @@ import 'package:flutter_svg/flutter_svg.dart';
 
 import 'index.dart';
 
-///
+/// This widget helps by forcing a size to a cached image,
+/// or anykind of widget, all around the app.
 class SizedImage extends StatelessWidget {
   static const num _smallSize = 40.0, _bigSize = 69.0;
 
@@ -17,12 +18,12 @@ class SizedImage extends StatelessWidget {
     this.onTap,
   });
 
-  ///
+  /// Leading parameter of a [ListCell] widget.
   factory SizedImage.list(String url) {
     return SizedImage(url: url, size: _smallSize);
   }
 
-  ///
+  /// Header of a [CardPage] widget.
   factory SizedImage.card(String url, {VoidCallback onTap}) {
     return SizedImage(url: url, size: _bigSize, onTap: onTap);
   }
