25a230143fa46c2d3a3aa4fea04c6166acd9a596
jesusrp98 <jesusrope98@outlook.com>
2020-04-19 13:12:12 +0200

    Updated radio cell

--- lib/ui/widgets/radio_cell.dart
+++ lib/ui/widgets/radio_cell.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-const _kRadioSize = 18.0;
+const _kRadioSize = 22.0;
 
 ///
 class RadioCell<T> extends StatelessWidget {
@@ -16,46 +16,26 @@ class RadioCell<T> extends StatelessWidget {
     this.onChanged,
   });
 
+  bool get isChecked => groupValue == value;
+
   @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () => onChanged(value),
       child: Container(
         padding: EdgeInsets.symmetric(horizontal: 20, vertical: 16),
-        color: groupValue == value
-            ? Theme.of(context).accentColor.withOpacity(0.24)
-            : null,
+        color:
+            isChecked ? Theme.of(context).accentColor.withOpacity(0.24) : null,
         child: Row(
           children: <Widget>[
-            Stack(
-              alignment: AlignmentDirectional.center,
-              children: <Widget>[
-                Container(
-                  height: _kRadioSize,
-                  width: _kRadioSize,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: groupValue == value
-                        ? Theme.of(context).accentColor
-                        : Theme.of(context)
-                            .textTheme
-                            .caption
-                            .color
-                            .withOpacity(0.20),
-                  ),
-                ),
-                if (groupValue == value)
-                  Container(
-                    height: _kRadioSize * 0.45,
-                    width: _kRadioSize * 0.45,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Theme.of(context).dialogBackgroundColor,
-                    ),
-                  ),
-              ],
+            Icon(
+              isChecked ? Icons.check_circle : Icons.panorama_fish_eye,
+              size: _kRadioSize,
+              color: isChecked
+                  ? Theme.of(context).accentColor
+                  : Theme.of(context).textTheme.caption.color,
             ),
-            SizedBox(width: 20),
+            SizedBox(width: 16),
             Text(
               title,
               style: GoogleFonts.rubikTextTheme(
