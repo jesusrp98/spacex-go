6ad1bdbb0b9f830f5f74af7ff350120e5f53b024
jesusrp98 <jesusrope98@outlook.com>
2021-01-19 16:37:43 +0100

    Updating cubit tests

--- test/cubits/changelog_test.dart
+++ test/cubits/changelog_test.dart
@@ -3,33 +3,21 @@ import 'package:cherry/cubits/base/index.dart';
 import 'package:cherry/cubits/index.dart';
 import 'package:cherry/repositories-cubit/changelog.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:mockito/mockito.dart';
+
+import 'helpers/hydrated.dart';
 
 class MockRepository extends Mock implements ChangelogRepository {}
 
-class MockStorage extends Mock implements Storage {}
-
 void main() {
-  TestWidgetsFlutterBinding.ensureInitialized();
-
+  initHydratedBloc();
   group('ChangelogCubit', () {
+    ChangelogCubit cubit;
     MockRepository repository;
-    Storage storage;
 
     setUp(() {
-      storage = MockStorage();
       repository = MockRepository();
-
-      // final response = MockResponse();
-
-      // when(response.statusCode).thenReturn(200);
-      // when(response.data).thenReturn(json);
-
-      HydratedCubit.storage = storage;
-
-      // when(storage.write(any, any)).thenAnswer((_) => Future.value(response));
-      // when(repository.loadData()).thenAnswer((realInvocation) => null);
+      cubit = ChangelogCubit(repository);
     });
 
     test('fails when null service is provided', () {
@@ -40,29 +28,25 @@ void main() {
       expect(ChangelogCubit(repository).state, RequestState<String>.init());
     });
 
-    // blocTest(
-    //   'fetches data correctly',
-    //   build: () {
-    //     when(service.getChangelog()).thenAnswer(
-    //       (_) => Future.value(
-    //         ModelReleaseRequest(
-    //           items: [
-    //             ModelRelease(
-    //               release: Release(
-    //                 models: [
-    //                   Model(id: '1'),
-    //                   Model(id: '2'),
-    //                 ],
-    //               ),
-    //             )
-    //           ],
-    //         ),
-    //       ),
-    //     );
-    //     return ChangelogCubit(service);
-    //   },
-    //   act: (cubit) => cubit.fetchData(),
-    //   expect: [RequestState.loading(), RequestState.loaded('Lorem')],
-    // );
+    group('toJson/fromJson', () {
+      test('work properly', () {
+        expect(
+          cubit.fromJson(cubit.toJson(cubit.state)),
+          cubit.state,
+        );
+      });
+    });
+
+    blocTest<ChangelogCubit, RequestState>(
+      'fetches data correctly',
+      build: () {
+        when(repository.fetchData()).thenAnswer(
+          (_) => Future.value('Lorem'),
+        );
+        return cubit;
+      },
+      act: (cubit) => cubit.loadData(),
+      expect: [RequestState.loading(), RequestState.loaded('Lorem')],
+    );
   });
 }
