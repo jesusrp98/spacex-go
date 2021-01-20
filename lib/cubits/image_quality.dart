1b0056a189fae9d20795797e5dc7646da84008a7
jesusrp98 <jesusrope98@outlook.com>
2021-01-20 18:34:33 +0100

    Added new cubits

---
+++ lib/cubits/image_quality.dart
@@ -0,0 +1,25 @@
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+
+enum ImageQuality { low, medium, high }
+
+class ImageQualityCubit extends HydratedCubit<ImageQuality> {
+  static const defaultQuality = ImageQuality.medium;
+
+  ImageQualityCubit() : super(defaultQuality);
+
+  @override
+  ImageQuality fromJson(Map<String, dynamic> json) {
+    return ImageQuality.values[json['value']];
+  }
+
+  @override
+  Map<String, int> toJson(ImageQuality state) {
+    return {
+      'value': state.index,
+    };
+  }
+
+  ImageQuality get imageQuality => state;
+
+  set imageQuality(ImageQuality imageQuality) => emit(imageQuality);
+}
